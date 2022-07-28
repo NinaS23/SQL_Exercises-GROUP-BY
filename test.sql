98ed50a161ffaa227422c50db9e2657adbb9c9fe
MariaDuda <mariabevenuto062@gmail.com>
2022-07-28 12:34:23 -0300

    feat exercise 3

--- test.sql
+++ test.sql
@@ -15,3 +15,14 @@ SELECT
 FROM educations e
     GROUP BY e."userId";
 `)
+
+--exercise 3 
+query(`
+SELECT 
+   u.name AS writer,
+   COUNT(t."recipientId") AS testimonailCount
+FROM testimonials t
+    JOIN users u ON u.id = t."writerId"
+    WHERE u.id=435
+    GROUP BY u.name
+`)
